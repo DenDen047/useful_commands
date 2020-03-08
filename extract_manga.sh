d1b4d50b60aba000b05471bf6634b3256aa676c4
DenDen047 <sh.mn.nat@gmail.com>
2020-03-08 09:37:00 +0900

    run the docker build command

--- extract_manga.sh
+++ extract_manga.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+IMAGE_NAME="denden047/useful_commands:latest"
 
+cd docker && \
+docker image build \
+    --rm \
+    -t ${IMAGE_NAME} .
+
+docker container run \
+    --rm \
+    --name bb \
+    ${IMAGE_NAME}
